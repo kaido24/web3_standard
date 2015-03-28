56e4764d905c1ec39269e8cef7648463a5dc4284
Kaido <toomingas.k@gmail.com>
2015-03-28 13:47:15 +0200

    fix sh file

--- buildsite.sh
+++ buildsite.sh
@@ -1,5 +1,5 @@
 mkdir profiles
 mv web3_standard profiles
 drush make profiles/web3_standard/local.make
-rm -rf sites/all/libraries/plupload/examples readme.md docs changelog.txt licence.txt
+rm -rf sites/all/libraries/plupload/examples
 rm buildsite.sh
